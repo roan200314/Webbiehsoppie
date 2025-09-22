0a741c4f28b6ab208e075b8307b4de02fde8e24c
hellyr <hellyr@hva.nl>
2025-09-22 21:24:14 +0200

    added: base grids

---
+++ src/app/footer/page.tsx
@@ -0,0 +1,25 @@
+export default function Footer() {
+    return (
+        <footer className="grid h-56 grid-cols-3">
+            <div className="bg-blue-600">
+                footer
+            </div>
+            <div className="bg-blue-600">
+                <p className="pt-10">Details</p>
+                <div>
+                    <p>1</p>
+                    <p>2</p>
+                    <p>3</p>
+                </div>
+            </div>
+            <div className="bg-blue-600">
+                <p className="pt-10">Details</p>
+                <div>
+                    <p>1</p>
+                    <p>2</p>
+                    <p>3</p>
+                </div>
+            </div>
+        </footer>
+    )
+}
---
+++ src/app/navbar/page.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav className="text-black">
+      <ul className="flex gap-4">
+        <li><a className="font-bold underline" href="#">Home</a></li>
+        <li><a className="font-bold underline" href="#">About</a></li>
+        <li><a className="font-bold underline" href="#">Faq</a></li>
+        <li><a className="font-bold underline" href="#">Whatever</a></li>
+      </ul>
+    </nav>
+  );
+}
